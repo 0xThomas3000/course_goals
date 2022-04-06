bd15ed2a6d69ca8d821fbe34f980c95558c0a36b
tuanit168 <tuanit168@gmail.com>
2022-04-06 16:45:27 +0700

    Installing expo-app-loading

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  useFonts({
+    'lalezar': require('./assets/fonts/Lalezar-Regular.ttf')
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
